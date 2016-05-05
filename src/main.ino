eaa35657685d0e8c865a9007eab922be75409cf1
Rob <robgerritsen11@gmail.com>
2016-05-05 21:08:59 +0200

    Data opslaan

--- src/main.ino
+++ src/main.ino
@@ -7,13 +7,16 @@
 #include <SPI.h>
 #include <SD.h>
 
+#include <SPI.h>
+#include <SD.h>
 long serialPort = 115200;
 DS3231 rtc(A4, A5);
 
+int SD_KAART_PIN = 4;
 void setup(){
   Serial.begin(serialPort);
   printf_begin();
-  opslag_init();
+  opslag_init(SD_KAART_PIN);
   klok_init();
 }
 
